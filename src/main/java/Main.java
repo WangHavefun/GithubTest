9ff2a4c8950457868d725013e7d05795ab66f5ba
WangHavefun <haifan.w@foxmail.com>
2022-10-30 21:54:21 +0800

    修改

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!！！");
         System.out.println("这是后来加的需要删掉的。");
+        System.out.println("随便加了点东西");
     }
 }
